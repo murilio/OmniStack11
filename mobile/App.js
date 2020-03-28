476e2d3c739fe059c36fd69e9eb0b496b5293500
murilio <muriliosilva201@gmail.com>
2020-03-28 17:16:32 -0300

    finalizando o app mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,12 @@
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Negada</Text>
-    </View>
+    <Routes/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f00',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
